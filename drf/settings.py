d806eab14970da558a051ea9346c1d2d1d3fd18f
davidSlva <davidsilvaurrutia@gmail.com>
2024-07-12 03:15:25 -0400

    settings

--- drf/settings.py
+++ drf/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.161.199.136', 'localhost', '127.0.0.1']
 
 
 # Application definition
